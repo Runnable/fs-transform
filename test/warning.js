f7d8bb6cae746d5904214c1f3cbe6efff49d97c1
Ryan Sandor Richards <ryan@runnable.com>
2016-02-23 15:27:36 -0800

    Standard format for warning tests.

--- test/warning.js
+++ test/warning.js
@@ -1,23 +1,19 @@
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var before = lab.before;
-var beforeEach = lab.beforeEach;
-var after = lab.after;
-var afterEach = lab.afterEach;
-var Code = require('code');
-var expect = Code.expect;
+var Lab = require('lab')
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
+var Code = require('code')
+var expect = Code.expect
 
-var Warning = require('../lib/warning.js');
+var Warning = require('../lib/warning.js')
 
-describe('Warning', function() {
-  it('should set the warning rule and message', function(done) {
-    var rule = { action: 'replace' };
-    var message = 'Warning message';
-    var warning = new Warning(rule, message);
-    expect(warning.rule).to.equal(rule);
-    expect(warning.message).to.equal(message);
-    done();
-  });
-});
+describe('Warning', () => {
+  it('should set the warning rule and message', (done) => {
+    var rule = { action: 'replace' }
+    var message = 'Warning message'
+    var warning = new Warning(rule, message)
+    expect(warning.rule).to.equal(rule)
+    expect(warning.message).to.equal(message)
+    done()
+  })
+})
